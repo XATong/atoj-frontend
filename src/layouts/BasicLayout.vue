436366dc29b346481b56703339ae262c0a103109
at <XK@javat.top>
2023-10-04 20:37:48 +0800

    初始化项目

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://gitee.com/x-2022-k" target="_blank">
+          编程导航OJ系统 by ATong
+        </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scope>
+#basicLayout {
+}
+
+#basicLayout .header {
+  background: red;
+  margin-bottom: 16px;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #bbb, #fff);
+  margin-bottom: 16px;
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+
+<script>
+import GlobalHeader from "@/components/GlobalHeader";
+
+export default {
+  components: { GlobalHeader },
+};
+</script>
