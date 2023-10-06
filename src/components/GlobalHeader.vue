d61ba4612ea8a6b9a907b5afc9579c1b28cf4a54
at <XK@javat.top>
2023-10-06 16:13:32 +0800

    全局状态管理初始化

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,30 +1,36 @@
 <template>
-  <div id="globalHader">
-    <a-menu
-      mode="horizontal"
-      :selected-keys="selectedKeys"
-      @menu-item-click="doMenuClick"
-    >
-      <a-menu-item
-        key="0"
-        :style="{ padding: 0, marginRight: '38px' }"
-        disabled
+  <a-row id="globalHader" style="margin-bottom: 16px" align="center">
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
       >
-        <div class="title-bar">
-          <img class="logo" src="../assets/R-C.png" />
-          <div class="title">领航 OJ</div>
-        </div>
-      </a-menu-item>
-      <a-menu-item v-for="item in routes" :key="item.path">
-        {{ item.name }}
-      </a-menu-item>
-    </a-menu>
-  </div>
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="title-bar">
+            <img class="logo" src="../assets/R-C.png" />
+            <div class="title">领航 OJ</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in routes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>{{ store.state.user?.loginUser?.userName ?? "未登录" }}</div>
+    </a-col>
+  </a-row>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import { useStore } from "vuex";
 import { routes } from "../router/routes";
 
 const router = useRouter();
@@ -42,6 +48,13 @@ const doMenuClick = (key: string) => {
     path: key,
   });
 };
+
+const store = useStore();
+setTimeout(() => {
+  store.dispatch("user/getLoginUser", {
+    userName: "阿通",
+  });
+}, 3000);
 </script>
 
 <style scoped>
